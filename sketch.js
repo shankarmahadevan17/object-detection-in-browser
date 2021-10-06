d7baf7acb734befd648d59a8670ef456b31290c1
Lakshmi Narasimhan <shagan170103@gmail.com>
2021-10-06 23:19:39 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,43 @@
+let video;
+let detector;
+let detections = [];
+
+function preload() {
+  
+  detector = ml5.objectDetector('cocossd');
+}
+
+function gotDetections(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  detections = results;
+  detector.detect(video, gotDetections);
+}
+
+function setup() {
+  createCanvas(640, 480);
+  
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+  detector.detect(video, gotDetections);
+}
+
+
+function draw() {
+  image(video, 0, 0);
+
+  for (let i = 0; i < detections.length; i++) {
+    let object = detections[i];
+    stroke(0, 255, 0);
+    strokeWeight(4);
+    noFill();
+    rect(object.x, object.y, object.width, object.height);
+    noStroke();
+    fill(0);
+    textSize(24);
+    text(object.label, object.x + 10, object.y + 24);
+  }
+}
+
